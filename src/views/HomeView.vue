c7de31a36512c5a7e273196b6e29ce7c953cd2fa
Vanessa Gonzalez <vanessagpro5@gmail.com>
2024-10-17 17:28:41 -0600

    first updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <h1>Hello</h1>
   </div>
 </template>
+<script setup>
+import { onMounted } from 'vue';
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+onMounted(() => {
+  document.title = "Mx Travel";
+})
 </script>
