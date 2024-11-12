d1eef4bd5a65c120747dfcbc6de660a84cb1804f
Vanessa Gonzalez <vanessagpro5@gmail.com>
2024-11-12 17:23:39 -0600

    footer changes

--- src/App.vue
+++ src/App.vue
@@ -15,22 +15,26 @@
     </div>
   </nav>
   <router-view/>
-  <footer class="w-full h-20 flex justify-evenly items-center bg-agency-blue">
-    <a href="mailto:ventasmxtravel@gmail.com" class="flex justify-center items-center h-fit">
-      <img class="w-9 h-9 mr-5" src="./assets/footer_icons/correo-electronico.png" alt="">
-      <h3 class="text-agency-white">ventasmxtravel@gmail.com</h3>
-    </a>
-    <a href="https://www.instagram.com/mxtravelcuu?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" class="flex justify-center items-center h-fit">
-      <img class="w-9 h-9 mr-5" src="./assets/footer_icons/instagram.png" alt="">
-      <h3 class="text-agency-white">@mxtravelcuu</h3>
-    </a>
-    <a href="https://www.facebook.com/agenciamxtravel" class="flex justify-center items-center h-fit">
-      <img class="w-9 h-9 mr-5" src="./assets/footer_icons/logo-de-facebook.png" alt="">
-      <h3 class="text-agency-white">MX Travel</h3>
-    </a>
-    <a href="https://www.tiktok.com/@mxtravelcuu?is_from_webapp=1&sender_device=pc" class="flex justify-center items-center h-fit">
-      <img class="w-9 h-9 mr-5" src="./assets/footer_icons/tik-tok.png" alt="">
-      <h3 class="text-agency-white">@mxtravelcuu</h3>
-    </a>
+  <footer class="w-full h-fit py-6 flex justify-center items-center bg-agency-blue md:flex-row">
+    <div class="flex w-5/12 flex-row justify-end lg:flex-row sm:flex-col sm:items-start sm:justify-evenly ">
+      <a href="mailto:ventasmxtravel@gmail.com" class="flex mb-4 justify-center items-center h-fit lg:mb-0">
+        <img class="w-6 h-6 mr-6 sm:mr-3" src="./assets/footer_icons/correo-electronico.png" alt="">
+        <p class="text-agency-white hidden sm:contents">ventasmxtravel@gmail.com</p>
+      </a>
+      <a href="https://www.instagram.com/mxtravelcuu?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" class="flex justify-center items-center h-fit">
+        <img class="w-6 h-6 mr-6 sm:mr-3" src="./assets/footer_icons/instagram.png" alt="">
+        <p class="text-agency-white hidden sm:contents">@mxtravelcuu</p>
+      </a>
+    </div>
+    <div class="flex w-2/6 flex-row lg:flex-row sm:flex-col sm:items-start sm:justify-evenly ">
+      <a href="https://www.facebook.com/agenciamxtravel" class="flex mb-4 lg:m-0 justify-center items-center h-fit">
+        <img class="w-6 h-6 mr-6 sm:mr-3"src="./assets/footer_icons/logo-de-facebook.png" alt="">
+        <p class="text-agency-white hidden sm:contents">MX Travel</p>
+      </a>
+      <a href="https://www.tiktok.com/@mxtravelcuu?is_from_webapp=1&sender_device=pc" class="flex justify-center items-center h-fit">
+        <img class="w-6 h-6 mr-6 sm:mr-3" src="./assets/footer_icons/tik-tok.png" alt="">
+        <p class="text-agency-white hidden sm:contents">@mxtravelcuu</p>
+      </a>
+    </div>
   </footer>
 </template>
